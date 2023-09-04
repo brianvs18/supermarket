50e0ff231767e8a40c9dbf3fd279ba4a4085fe31
Brian Vanegas <jhon.vanegas@sofka.com.co>
2023-09-03 22:31:07 -0500

    implements logs in use case

--- domain/usecase/src/main/java/com/softlond/usecase/saleusecase/SaleHandlerUseCase.java
+++ domain/usecase/src/main/java/com/softlond/usecase/saleusecase/SaleHandlerUseCase.java
@@ -6,13 +6,18 @@ import com.softlond.model.saledetail.gateways.SaleDetailRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 
+import java.util.logging.Logger;
+
 @RequiredArgsConstructor
 public class SaleHandlerUseCase {
 
     private final SaleRepository saleRepository;
     private final SaleDetailRepository saleDetailRepository;
 
+    Logger log = Logger.getLogger(SaleHandlerUseCase.class.getName());
+
     public Flux<Sale> findAllSales() {
+        log.info("*** ENTER TO SaleHandlerUseCase :: findAllSales");
         return null;
     }
 }
